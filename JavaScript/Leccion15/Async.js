8cb6753d7d348121dfec7feb64afa4804af267e7
Ellin-cenciado <ellinsb2k21@gmail.com>
2023-10-10 20:19:10 -0300

    updated index.html and formatted Async.js

--- JavaScript/Leccion15/Async.js
+++ JavaScript/Leccion15/Async.js
@@ -1,6 +1,6 @@
 //async indica que una funcion devuelve una promesa
-async function miFuncionPromesa(){
-    return 'Funcion con async'
+async function miFuncionPromesa() {
+    return "Funcion con async";
 }
 
 //miFuncionPromesa().then(valor => console.log(valor));
@@ -46,29 +46,29 @@ So, this piece of code is a simulation of an asyncronous task, whereas "value" i
 multiple attempts at changing this value to true, if it succedes, then "everything's fine" is going to be printed
 if it fails, it will try again 4 times before giving up and printing an error message.
 */
-async function AsyncFun(value){
+async function AsyncFun(value) {
     let trueValue = value;
-    const promesa = new Promise((resolve,reject) => {
+    const promesa = new Promise((resolve, reject) => {
         let i = 0;
         const attemptChangeValue = () => {
-            if( i < 4 && !trueValue){
-                console.log(`Attempting to change value: ${i+1}/4 att.`);
+            if (i < 4 && !trueValue) {
+                console.log(`Attempting to change value: ${i + 1}/4 att.`);
                 trueValue = false; //Change this to true and everything's gonna be fine
                 i++;
-                setTimeout(attemptChangeValue,1000);
-            }else{
-                if(!trueValue){
-                    reject('Something went wrong');
-                }else {
-                    resolve('Everything is fine!');
+                setTimeout(attemptChangeValue, 1000);
+            } else {
+                if (!trueValue) {
+                    reject("Something went wrong");
+                } else {
+                    resolve("Everything is fine!");
                 }
             }
         };
-        setTimeout(attemptChangeValue,0);
+        setTimeout(attemptChangeValue, 0);
     });
-    try{
+    try {
         console.log(await promesa);
-    }catch(error){
+    } catch (error) {
         console.log(error);
     }
 }
